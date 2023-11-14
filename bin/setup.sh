47eb1571e775df050586eb5c35cb6371cd030c3b
Abul Syed <abul.syed@HMCTS.net>
2023-11-14 11:53:34 +0000

    add user setup scripts

---
+++ bin/setup.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# Setup Services
+#echo "Setting up Services..."
+#./add-idam-clients.sh
+
+# Setup Roles
+echo ""
+echo "Setting up Roles..."
+#./utils/idam-add-role.sh "ccd-import"
+#./utils/idam-add-role.sh "caseworker"
+#./utils/idam-add-role.sh "caseworker-iac"
+#./utils/idam-add-role.sh "cwd-user"
+#./utils/idam-add-role.sh "cwd-system-user"
+#./utils/idam-add-role.sh "cwd-admin"
+
+### Setup Users
+echo ""
+echo "Setting up Users..."
+#./bin/create-user.sh "ccd.docker.default@hmcts.net" "Default" "CCD_Docker" "Pa55word11" "ccd-import" "[{ \"code\": \"ccd-import\"}]"
+#./bin/create-user.sh "am.docker.default@hmcts.net" "Default" "AM_Docker" "Pa55word11" "ccd-import" "[{ \"code\": \"ccd-import\"}]"
+./bin/create-user.sh "ras.admin@hmcts.net" "testfirstname" "testsurname" "Pa55word11" "test" "[{ \"code\": \"caseworker\"}]"
+./bin/create-user.sh "orm.admin@hmcts.net" "testfirstname" "testsurname" "Pa55word11" "test" "[{ \"code\": \"caseworker\"}]"
+./bin/create-user.sh "TEST_AM_USER1_BEFTA@test.local" "testfirstname" "testsurname" "Pa55word11" "test" "[{ \"code\": \"caseworker\"}]"
+./bin/create-user.sh "TEST_AM_USER2_BEFTA@test.local" "testfirstname" "testsurname" "Pa55word11" "test" "[{ \"code\": \"caseworker\"}]"
+./bin/create-user.sh "TEST_AM_USER3_BEFTA@test.local" "testfirstname" "testsurname" "Pa55word11" "test" "[{ \"code\": \"caseworker\"}]"
+./bin/create-user.sh "TEST_AM_USER4_BEFTA@test.local" "testfirstname" "testsurname" "Pa55word11" "test" "[{ \"code\": \"caseworker\"}, { \"code\": \"cwd-admin\"}]"
+./bin/create-user.sh "TEST_AM_USER7_BEFTA@test.local" "testfirstname" "testsurname" "Pa55word11" "test" "[{ \"code\": \"caseworker\"}, { \"code\": \"cwd-system-user\"}]"
+./bin/create-user.sh "TEST_AM_USER6_BEFTA@test.local" "testfirstname" "testsurname" "Pa55word11" "test" "[{ \"code\": \"caseworker\"}]"
+
+# to use
+# 1. ./bin/setup.sh
+# 2. echo "Bearer $(./bin/utils/idam-user-token.sh TEST_AM_USER1_BEFTA@test.local)" | pbcopy
